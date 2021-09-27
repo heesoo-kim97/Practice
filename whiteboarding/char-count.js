51256d6001457f14eb66f9e3ea773ecaeaf863c9
heesoo-kim97 <heesoo0323@gmail.com>
2021-09-27 17:32:56 +0000

    Working on level 4 and 5.

--- whiteboarding/char-count.js
+++ whiteboarding/char-count.js
@@ -1,8 +1,11 @@
 function charCounts(string) {
   const newObject = {};
   for (let i = 0; i < string.length; i++) {
-    if (string[i] !== newObject[string[i]])
+    if (string[i] !== newObject[string[i]]) {
       newObject[string[i]] = 1;
+    } else if (string[i] === newObject[string[i]]) {
+      newObject[string[i]]++;
+    }
   }
-  return newObject
+  return newObject;
 }
--- whiteboarding/get-in.js
+++ whiteboarding/get-in.js
@@ -1,4 +1,12 @@
 function getIn(path, data) {
-  console.log(path);
-  console.log(data);
+  let level = 1;
+  for(let i = 0; i < path.length; i++) {
+    if (path[i] === '.') {
+      level++;
+    }
+  }
+  if (level === 2 && data[path] === {}) {
+    return data[path];
+  }
+  return data[path];
 }
